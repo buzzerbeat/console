5d8dbaa05900de0d73ca40d23c48892447f05750
jobbr <jobbr@qq.com>
2016-08-06 16:11:17 +0800

    yxdev

--- controllers/UpdateController.php
+++ controllers/UpdateController.php
@@ -6,6 +6,7 @@ use yii\console\Controller;
 
 use backend\models\microvideo\MvTag;
 use backend\models\microvideo\MvVideoTagRel;
+use common\models\Image;
 
 class UpdateController extends controller{
     /*
@@ -27,4 +28,15 @@ class UpdateController extends controller{
          
         echo "更新标签{$num}个，耗时" . round((microtime(true)-$sTime)*10)/10 . "秒\n";
     }
+
+	public function actionDeleteNoFileImg() {
+		$imgs = Image::find()->where('id%10 = 0')->all();
+		foreach ($imgs as $img) {
+			$file = yii::$app->params['imgDir'] . $img->file_path;
+			if (!file_exists($file)) {
+				echo "$file\n";
+				$img->delete();
+			}
+		}
+	}
 }
